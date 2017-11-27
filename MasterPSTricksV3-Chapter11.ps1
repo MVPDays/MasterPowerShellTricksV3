fa26f4ac9bff773a2b6fd8953bd8910ea12c9b48
dkawula <dkawula@triconelite.com>
2017-11-27 14:48:00 -0700

    PowerShell Rules for Format-Table and Format-List 
    In PowerShell, when outputting data to the console, it’s typically
    either organized into a table or a list. You can force output to take
    either of these forms using the Format-Table and the Format-List
    cmdlets, and people who write PowerShell cmdlets and modules can take
    special steps to make sure their output is formatted as they desire.
    But, when no developer has specifically asked for a formatted output,
    how does PowerShell choose to display a table or a list?

--- MasterPSTricksV3-Chapter11.ps1
+++ MasterPSTricksV3-Chapter11.ps1
@@ -1,27 +1,20 @@
-﻿
-#Recently, I was helping someone in a forum who was trying to figure out what kind of object their command was returning. They knew about the standard cmdlets people suggest when you’re getting started (Get-Help, Get-Member, and Get-Command), but couldn’t figure out what was coming back from a specific command. 
-#In order to make this a more generic example, and to simplify it, let’s approach this differently. Say I have these two objects where one is a string and the other is an array of two strings. 
+﻿<#>
+In PowerShell, when outputting data to the console, it’s typically either organized into a table or a list. You can force output to take either of these forms using the Format-Table and the Format-List cmdlets, and people who write PowerShell cmdlets and modules can take special steps to make sure their output is formatted as they desire. But, when no developer has specifically asked for a formatted output, how does PowerShell choose to display a table or a list? 
+
+The answer is actually pretty simple and I’m going to highlight it with an example. Take a look at the following piece of code. 
+
+</#>
+
+get-wmiobject -class win32_operatingsystem | select pscomputername,caption,osarch*,registereduser 
 
 
-$thing1 = 'This is an item' 
-$thing2 = @('This is another item','This is one more item') 
-$thing1; $thing2  
+#I used Get-WmiObject to get some information about my operating system. I selected four properties and PowerShell decided to display a table. Now, let’s add another property to return. 
 
-#The third line shows you what you get if you write these out to the screen. 
+get-wmiobject -class win32_operatingsystem | select pscomputername,caption,osarch*,registereduser,version
 
-#It looks like three separate strings, right? Well we should be able to dissect these with Get-Member to get to the bottom of this and identify the types of objects these are. After all, one is a string and the other is an array, right?  
-
-$thing1 | Get-Member 
-
-#Dang, $thing2 is an array but Get-Member is still saying the TypeName is System.String. What’s going on? 
-#Well, the key here is what we’re doing is writing the output of $thing2 into Get-Member. So the output of $thing2 is two strings, and that’s what’s actually hitting Get-Member. If we want to see what kind of object $thing2 really is, we need to use a method that’s built into every PowerShell object: GetType(). 
-
-$thing2.GetType()  
-
-#There you go. $thing2 is a System.Array object, just like we thought. 
-
-
-
-
-
-
+<#>Whoa, now we get a list. What gives? 
+Well here’s how PowerShell decides, by default, whether to display a list or table: 
+·	If showing four or fewer properties, show a table 
+·	If showing five or more properties, show a list 
+That’s it, that’s how PowerShell decides by default whether to show you a list or table. 
+</#>
