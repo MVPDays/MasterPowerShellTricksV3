d169ae5749435050a809d6dda108ee3c98d1c9c5
dkawula <dkawula@triconelite.com>
2017-11-27 15:12:12 -0700

    Opening an Exchange Online Protection Shell
    I built a PowerShell function in my profile to connect quickly to
    Exchange Online. That’s great, but what if you also want to manage
    Exchange Online Protection (EOP) from a PoweShell console? Well it turns
    out to be pretty easy.

---
+++ MasterPSTricksV3-Chapter27.ps1
@@ -0,0 +1,3 @@
+﻿$cred = Get-Credential 
+$s = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection 
+import-pssession $s  
