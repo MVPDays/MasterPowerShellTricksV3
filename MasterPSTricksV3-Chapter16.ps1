afaa1c1cbcbfe0baaee4573cc644989a9091ea9a
dkawula <dkawula@triconelite.com>
2017-11-27 14:57:07 -0700

    Get a ServiceNow User Using PowerShell 
    Ever wanted to work with ServiceNow via PowerShell?   Let me show you
    some basics like fetching a user.

---
+++ MasterPSTricksV3-Chapter16.ps1
@@ -0,0 +1,17 @@
+﻿#Ever wanted to work with ServiceNow via PowerShell?   Let me show you some basics like fetching a user.
+#Let’s jump into some code first and I’ll break down what I’m doing. 
+
+
+$user = $Credential.Username 
+$pass = $Credential.GetNetworkCredential().Password 
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $user, $pass))) 
+  
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]" 
+$headers.Add('Authorization',('Basic {0}' -f $base64AuthInfo)) 
+$headers.Add('Accept','application/json') 
+  
+$uri = "https://$SubscriptionSubDomain.service-now.com/api/now/v1/table/sys_user?sysparm_query=user_name=$Username" 
+  
+$response = Invoke-WebRequest -Headers $headers -Method "GET" -Uri $uri  
+$result = ($response.Content | ConvertFrom-Json).Result  
+
