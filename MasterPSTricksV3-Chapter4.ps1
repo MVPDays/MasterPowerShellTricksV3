310604bec2a9c490fe16dd647207961f910d6d9f
dkawula <dkawula@triconelite.com>
2017-11-27 14:22:00 -0700

    Setting SQL Server Memory Allocation Maximum and Minimum with PowerShell
    If you’ve ever run an installation of SQL Server, you’ll know it’s a
    database, and databases love, love, love memory. By design and by
    default, Microsoft SQL Server thinks it’s the only process on the system
    and is therefore given all the available memory and CPUs. As a best
    practice, I limit this. Here is the script I use to edit these values.

---
+++ MasterPSTricksV3-Chapter4.ps1
@@ -0,0 +1,49 @@
+﻿Function Get-ComputerMemory { 
+    $mem = Get-WMIObject -class Win32_PhysicalMemory | 
+Measure-Object -Property Capacity -Sum 
+    return ($mem.Sum / 1MB); 
+}  
+
+
+Function Get-SQLMaxMemory {  
+    $memtotal = Get-ComputerMemory 
+    $min_os_mem = 2048 ; 
+    if ($memtotal -le $min_os_mem) { 
+        Return $null; 
+    } 
+    if ($memtotal -ge 8192) { 
+        $sql_mem = $memtotal - 2048 
+    } else { 
+        $sql_mem = $memtotal * 0.8 ; 
+    } 
+    return [int]$sql_mem ;   
+}  
+
+
+Function Set-SQLInstanceMemory { 
+    param ( 
+        [string]$SQLInstanceName = ".",  
+        [int]$maxMem = $null,  
+        [int]$minMem = 0 
+    ) 
+  
+    if ($minMem -eq 0) { 
+        $minMem = $maxMem 
+    } 
+    [reflection.assembly]::LoadWithPartialName("Microsoft.SqlServer.Smo") | Out-Null 
+    $srv = New-Object Microsoft.SQLServer.Management.Smo.Server($SQLInstanceName) 
+    if ($srv.status) { 
+        Write-Host "[Running] Setting Maximum Memory to: $($srv.Configuration.MaxServerMemory.RunValue)" 
+        Write-Host "[Running] Setting Minimum Memory to: $($srv.Configuration.MinServerMemory.RunValue)" 
+  
+        Write-Host "[New] Setting Maximum Memory to: $maxmem" 
+        Write-Host "[New] Setting Minimum Memory to: $minmem" 
+        $srv.Configuration.MaxServerMemory.ConfigValue = $maxMem 
+        $srv.Configuration.MinServerMemory.ConfigValue = $minMem    
+        $srv.Configuration.Alter() 
+    } 
+}  
+
+
+$MSSQLInstance = "sql01\SQLInstance01" 
+Set-SQLInstanceMemory $MSSQLInstance (Get-SQLMaxMemory) 
