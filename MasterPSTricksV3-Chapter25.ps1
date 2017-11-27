89deacc163950d4648e1a3868455fc5ce019b2e8
dkawula <dkawula@triconelite.com>
2017-11-27 15:10:10 -0700

    Using Select-Object to Explore Objects
    When you’re first getting started with PowerShell, you may not be aware
    that sometimes when you run a command to get data, the information
    returned to the screen is not ALL the information that the command
    actually returned.

---
+++ MasterPSTricksV3-Chapter25.ps1
@@ -0,0 +1,6 @@
+﻿#Let me clarify with an example. If you run the Get-ChildItem cmdlet, you’ll get a bit of information back about all the files in whichever directory you specified. 
+
+Get-ChildItem c:\temp\demo 
+Get-ChildItem c:\temp\demo | Select-Object -Property *  
+
+Get-ChildItem c:\temp\demo | Select-Object -Property Name, Attributes, IsReadOnly  
