bdf3ff3f78282e75d6e8775b720649302c0d1e1a
dkawula <dkawula@triconelite.com>
2017-11-27 14:53:55 -0700

    Piping PowerShell Output into Bash 
    With Windows 10, you can install Bash on Windows. Cool, right? Having
    Bash on Windows goes a long way towards making Windows a more
    developer-friendly environment and opens a ton of doors. The one I’m
    going to show you today is more of a novelty than anything else, but
    maybe you’ll find something neat to do with it.

---
+++ MasterPSTricksV3-Chapter14.ps1
@@ -0,0 +1,3 @@
+﻿#If you’ve been around PowerShell, you’re used to seeing the pipe character ( | ) used to pass the output from one command into the input of another. What you can do now, kind of, is pass the output of a PowerShell command into the input of a Bash command. Here’s an example. Get ready for this biz. 
+
+Get-ChildItem c:\temp\demo | foreach-object { bash -c "echo $($_.Name) | awk /\.csv/" }  
