3425fb098d6683411d9630d37f25f22ea27e5a1f
dkawula <dkawula@triconelite.com>
2017-11-27 15:08:53 -0700

    Using Get-Member to Explore Objects
    I previously wrote about using Select-Object to explore PowerShell
    objects. Now, I am going to quickly cover using Get-Member to do the
    same.

---
+++ MasterPSTricksV3-Chapter24.ps1
@@ -0,0 +1,10 @@
+﻿#Let’s say you’re using Get-CimInstance to get information about the operating system. You might do something like this. 
+
+Get-CimInstance -ClassName win32_operatingsystem 
+
+Get-CimInstance -ClassName win32_operatingsystem | get-member 
+
+
+$osInfo = Get-CimInstance -ClassName win32_operatingsystem 
+
+$osInfo.GetHashCode() 
