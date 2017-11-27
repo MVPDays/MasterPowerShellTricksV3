e3f4e860974fb0320712195cfa52bfaff12053d5
dkawula <dkawula@triconelite.com>
2017-11-27 15:11:02 -0700

    Can PowerShell Parameters Belong to Multiple Sets?
    Say you’ve got a function that takes three parameters: Username,
    ComputerName and SessionName, but you don’t want someone to use
    ComputerName and SessionName at once. You decide to put them in separate
    parameter sets. Awesome, except you want Username to be a part of both
    parameter sets and it doesn’t look like you can specify more than one.

---
+++ MasterPSTricksV3-Chapter26.ps1
@@ -0,0 +1,26 @@
+﻿function Do-Thing { 
+    [CmdletBinding()] 
+    param ( 
+    [Parameter( ParameterSetName = 'Computer','Session' )][string]$Username, 
+    [Parameter( ParameterSetName = 'Computer' )][string]$ComputerName, 
+    [Parameter( ParameterSetName = 'Session' )][PSSession]$SessionName 
+    ) 
+# Other code 
+}  
+
+#So how do you make a parameter a member of more than one parameter set? You need more [Parameter()] qualifiers. 
+
+function Do-Thing { 
+    [CmdletBinding()] 
+    param ( 
+    [Parameter( ParameterSetName = 'Computer' )] 
+    [Parameter( ParameterSetname = 'Session' )] 
+    [string]$Username, 
+  
+    [Parameter( ParameterSetName = 'Computer' )][string]$ComputerName, 
+    [Parameter( ParameterSetName = 'Session' )][PSSession]$SessionName 
+    ) 
+# Other code 
+}  
+
+
