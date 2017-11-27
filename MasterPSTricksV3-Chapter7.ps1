1c694ebd866878fbef28c07aeb3c093e8b6624c4
dkawula <dkawula@triconelite.com>
2017-11-27 14:30:54 -0700

    Using PowerShell to Update the .Default and All User Profiles Registry
    There are times that you may need to push out a change to all existing
    user profiles and to new profiles that are created on a system. I’ve
    seen a few PowerShell scripts floating around out there, but they didn’t
    seem to work for Windows 7 SP1. You may or may not be surprised, but
    there are many organizations that still run Windows 7. The script is
    actually pretty simple.

---
+++ MasterPSTricksV3-Chapter7.ps1
@@ -0,0 +1,38 @@
+﻿# Get each user profile SID and Path to the profile 
+$UserProfiles = Get-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList\*" | Where {$_.PSChildName -match "S-1-5-21-(\d+-?){4}$" } | Select-Object @{Name="SID"; Expression={$_.PSChildName}}, @{Name="UserHive";Expression={"$($_.ProfileImagePath)\NTuser.dat"}}  
+
+# Add in the .DEFAULT User Profile 
+$DefaultProfile = "" | Select-Object SID, UserHive 
+$DefaultProfile.SID = ".DEFAULT" 
+$DefaultProfile.Userhive = "C:\Users\Public\NTuser.dat" 
+$UserProfiles += $DefaultProfile  
+
+# Loop through each profile on the machine</p> 
+Foreach ($UserProfile in $UserProfiles) { 
+    # Load User ntuser.dat if it's not already loaded 
+    If (($ProfileWasLoaded = Test-Path Registry::HKEY_USERS\$($UserProfile.SID)) -eq $false) { 
+        Start-Process -FilePath "CMD.EXE" -ArgumentList "/C REG.EXE LOAD HKU\$($UserProfile.SID) $($UserProfile.UserHive)" -Wait -WindowStyle Hidden 
+    }  
+
+
+# Manipulate the registry 
+$key = "Registry::HKEY_USERS\$($UserProfile.SID)\Software\SomeArchaicSoftware\Configuration" 
+New-Item -Path $key -Force | Out-Null 
+New-ItemProperty -Path $key -Name "LoginURL" -Value "https://www.myCompany.local" -PropertyType STRING -Force | Out-Null 
+New-ItemProperty -Path $key -Name "DisplayWelcome" -Value 0x00000001 -PropertyType DWORD -Force | Out-Null 
+  
+$key = "$key\UserInfo" 
+New-Item -Path $key -Force | Out-Null 
+New-ItemProperty -Path $key -Name "LoginName" -Value "$($ENV:USERDOMAIN)\$($ENV:USERNAME)" -PropertyType STRING -Force | Out-Null 
+
+
+# Unload NTuser.dat         
+If ($ProfileWasLoaded -eq $false) { 
+    [gc]::Collect() 
+    Start-Sleep 1 
+    Start-Process -FilePath "CMD.EXE" -ArgumentList "/C REG.EXE UNLOAD HKU\$($UserProfile.SID)" -Wait -WindowStyle Hidden| Out-Null 
+}  
+
+
+
+
