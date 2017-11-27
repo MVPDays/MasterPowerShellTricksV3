c3ff3e013a1bbd924cbf9a305e99b653a019bb84
dkawula <dkawula@triconelite.com>
2017-11-27 15:21:27 -0700

    Azure Automatic Account Creation and Adding Modules using PowerShell
    Microsoft's Operations Management Suite provides some exceptional tools
    for monitoring and maintaining your environments in both the cloud and
    in your datacenter.  One of it's best features, however, is its ability
    to leverage the tools that you've already developed to perform tasks and
    remediate issues using PowerShell, Azure Automation Runbooks, and OMS
    Alert triggers.  In this series, we'll be discussing how you can
    configure these tools to take care of problems in your own environment.
    Today, we'll be talking about how you can take your own PowerShell
    Modules and upload them to Azure Automation.

---
+++ MasterPSTricksV3-Chapter33.ps1
@@ -0,0 +1,48 @@
+﻿Get-AzureRmResourceProvider -ProviderNamespace 'Microsoft.Automation' 
+
+$BaseName = 'testautoacct'
+$Location = 'eastus2'
+$ResGrp = New-AzureRmResourceGroup -Name $BaseName -Location $Location -Verbose
+$AutoAcct = New-AzureRmAutomationAccount -ResourceGroupName $ResGrp.ResourceGroupName -Name ($BaseName + $Location) -Location $ResGrp.Location 
+
+
+
+
+$Stor = New-AzureRmStorageAccount -ResourceGroupName $ResGrp.ResourceGroupName -Name modulestor -SkuName Standard_LRS -Location $ResGrp.Location -Kind BlobStorage -AccessTier Hot
+
+Add-AzureAccount
+$Subscription = ((Get-AzureSubscription).where({$PSItem.SubscriptionName -eq 'LastWordInNerd'})) 
+Select-AzureSubscription -SubscriptionName $Subscription.SubscriptionName -Current
+
+$StorKey = (Get-AzureRmStorageAccountKey -ResourceGroupName $Stor.ResourceGroupName -Name $Stor.StorageAccountName).where({$PSItem.KeyName -eq 'key1'})
+$StorContext = New-AzureStorageContext -StorageAccountName $Stor.StorageAccountName -StorageAccountKey $StorKey.Value
+
+
+
+
+
+$Container = New-AzureStorageContainer -Name 'modules' -Permission Blob -Context $StorContext -Permission Blob 
+
+
+
+
+$ModuleLoc = 'C:\Scripts\Presentations\OMSAutomation\Modules\'
+$Modules = Get-ChildItem -Directory -Path $ModuleLoc
+    
+    ForEach ($Mod in $Modules){
+
+        Compress-Archive -Path $Mod.PSPath -DestinationPath ($ModuleLoc + '\' + $Mod.Name + '.zip') -Force
+
+    }
+
+$ModuleArchive = Get-ChildItem -Path $ModuleLoc -Filter "*.zip"
+
+ForEach ($Mod in $ModuleArchive){
+        
+    $Blob = Set-AzureStorageBlobContent -Context $StorContext -Container $Container.Name -File $Mod.FullName -Force -Verbose
+    New-AzureRmAutomationModule -ResourceGroupName $ResGrp.ResourceGroupName -AutomationAccountName $AutoAcct.AutomationAccountName -Name ($Mod.Name).Replace('.zip','') -ContentLink $Blob.ICloudBlob.Uri.AbsoluteUri
+
+} 
+
+
+
