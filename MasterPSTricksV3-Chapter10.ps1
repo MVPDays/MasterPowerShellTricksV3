508f7fb170406f318bb5eea94ba82a9006b98550
dkawula <dkawula@triconelite.com>
2017-11-27 14:51:23 -0700

    What's the difference between -split and .split() in PowerShell?
    Here’s a question I see over and over and over again: “I have a string
    and I’m trying to split it on this part, but it’s jumbling it into a big
    mess. What’s going on?” Well, there’s splitting a string in PowerShell,
    and then there’s splitting a string in PowerShell. Confused? Let me
    explain.
    Say you have this string for our example.

---
+++ MasterPSTricksV3-Chapter10.ps1
@@ -0,0 +1,43 @@
+﻿$splitstring = 'this is an interesting string with the letters s and t all over the place' 
+
+$splitstring.split('s') 
+
+<#>  
+thi 
+ i 
+ an intere 
+ting 
+tring with the letter 
+  
+ and t all over the place  
+</#>
+
+
+$splitstring.split('st') 
+  
+<#>  
+hi 
+ i 
+ an in 
+ere 
+  
+ing 
+  
+ring wi 
+h 
+he le 
+  
+er 
+  
+ and 
+ all over 
+he place  
+</#>
+
+
+$splitstring -split 'st' 
+<#>  
+this is an intere 
+ing  
+ring with the letters s and t all over the place  
+</#>
