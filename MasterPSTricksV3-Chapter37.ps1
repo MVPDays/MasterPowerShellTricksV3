be02502c210e6e35d8081c6f01d889c61070a875
dkawula <dkawula@triconelite.com>
2017-11-27 15:32:22 -0700

    Onboarding Automation DSC Endpoints and Reporting
    In the last chapter we talked about modifying and uploading our
    configurations to Azure Automation DSC.  We were able to import
    credentials from Azure's Automation Account Credential store, and then
    compile the .mof files in the automation account for deployment.  In
    this chapter, we'll be looking at how we apply those configurations to
    existing systems via PowerShell.  Then we'll take a look at some of the
    reporting available via Azure Automation DSC and send those reports over
    to Operations Management Suite for dashboarding.

---
+++ MasterPSTricksV3-Chapter37.ps1
@@ -0,0 +1,65 @@
+﻿Get-AzureRmAutomationDscNodeConfiguration -ResourceGroupName $AutoAcct.ResourceGroupName -AutomationAccountName $AutoAcct.AutomationAccountName -ConfigurationName TestConfig
+
+
+
+$TargetResGroup = 'nrdtste'
+$VMName = 'ctrxeusdbnp01'
+
+$VM = Get-AzureRmVM -ResourceGroupName $TargetResGroup -Name $VMName 
+
+
+
+
+
+$DSCLCMConfig = @{
+
+    'ConfigurationMode' = 'ApplyAndAutocorrect'
+    'RebootNodeIfNeeded' = $true
+    'ActionAfterReboot' = 'ContinueConfiguration'
+
+} 
+
+
+
+#Once we have all of this, we can now go ahead and register our target node in Automation DSC using the Register-AzureRmAutomationDscNode command.
+
+Register-AzureRmAutomationDscNode -AzureVMName $VM.Name -AzureVMResourceGroup $VM.ResourceGroupName -AzureVMLocation $VM.Location -AutomationAccountName $AutoAcct.AutomationAccountName -ResourceGroupName $AutoAcct.ResourceGroupName @DSCLCMConfig 
+
+
+
+
+$Configuration = Get-AzureRmAutomationDscNodeConfiguration -AutomationAccountName $AutoAcct.AutomationAccountName -ResourceGroupName $AutoAcct.ResourceGroupName -Name 'CompositeConfig.webServer'
+
+$TargetNode = Get-AzureRmAutomationDscNode -Name $VM.Name -ResourceGroupName $AutoAcct.ResourceGroupName -AutomationAccountName $AutoAcct.AutomationAccountName
+Set-AzureRmAutomationDscNode -Id $TargetNode.Id -NodeConfigurationName $Configuration.Name -AutomationAccountName $AutoAcct.AutomationAccountName -ResourceGroupName $AutoAcct.ResourceGroupName -Verbose -Force
+
+
+
+Get-AzureRmAutomationDscNodeReport -NodeId $TargetNode.Id -ResourceGroupName $AutoAcct.ResourceGroupName -AutomationAccountName $AutoAcct.AutomationAccountName -Latest 
+
+
+
+
+
+
+
+#Get the resourceId of the automation account.
+$AutoAcctResource = Find-AzureRmResource -ResourceType "Microsoft.Automation/automationAccounts" -ResourceNameContains 'testautoaccteastus2'
+
+#Get the resourceId of the Log Analytics Workspace
+$LogAnalyticsResource = Find-AzureRmResource -ResourceType "Microsoft.OperationalInsights/workspaces" -ResourceNameContains 'LWINerd' 
+
+
+#Then we can use those resourceIds to pass to Set-AzureRmDiagnosticSetting and specify our DSCNodeStatus category.
+
+
+Set-AzureRmDiagnosticSetting -ResourceId $AutoAcctResource.ResourceId -WorkspaceId $LogAnalyticsResource.ResourceId -Enabled $true -Categories "DscNodeStatus" -Verbose 
+
+
+
+#Then you'll get a return similar to this:
+
+Set-AzureRmDiagnosticSetting -ResourceId $AutoAcctResource.ResourceId -WorkspaceId $LogAnalyticsResource.ResourceId -Enabled $true -Categories "D
+scNodeStatus" -Verbose
+
+
