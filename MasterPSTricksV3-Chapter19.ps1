f756818ee68caa0deb858d3fa681abf8886cdf84
dkawula <dkawula@triconelite.com>
2017-11-27 15:02:38 -0700

    Add a Column to a CSV using PowerShell
    Say you have a CSV file full of awesome, super great, amazing
    information. It’s perfect, except it’s missing a column. Luckily, you
    can use Select-Object along with the other CSV cmdlets to add a column.

---
+++ MasterPSTricksV3-Chapter19.ps1
@@ -0,0 +1,16 @@
+﻿#In our example, let’s say that you have a CSV with two columns “ComputerName” and “IPAddress” and you want to add a column for “Port3389Open” to see if the port for RDP is open or not. It’s only a few lines of code from being done. 
+
+$servers = Import-Csv C:\Temp\demo\servers.csv 
+
+$servers 
+
+
+$servers = $servers | Select-Object -Property *, @{label = 'Port3389Open'; expression = {(Test-NetConnection -ComputerName $_.Name -Port 3389).TcpTestSucceeded}}  
+
+
+$servers | Export-Csv -Path c:\temp\demo\servers-and-port-data.csv -NoTypeInformation  
+$Servers 
+
+
+
+
