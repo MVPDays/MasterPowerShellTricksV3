bcea80959c86cb82db63e859b468e150e3e1ad49
dkawula <dkawula@triconelite.com>
2017-11-27 15:33:50 -0700

    Publishing Configurations and Pushing them with Azure DSC
    So, we've talked about Azure Automation DSC and the extensive reporting
    we can get from it.  With the pricing as it is, it would be hard to
    argue as to why you would want to use anything else.  But I'm a
    completionist, and there may be some edge cases that might come up where
    you wouldn't be able to use the pull method for configurations.  So
    let's talk about how you can use Azure DSC to push a configuration to a
    virtual machine.

---
+++ MasterPSTricksV3-Chapter38.ps1
@@ -0,0 +1,29 @@
+﻿$AutoResGrp = Get-AzureRmResourceGroup -Name 'mms-eus'
+$StorAcct = Get-AzureRmStorageAccount -ResourceGroupName $AutoResGrp.ResourceGroupName -Name 'modulestor'
+
+
+#Now that we have our private store, we're going to publish our configuration using the Publish-AzureRmVMDscConfiguration command.
+
+
+$DSCBlob = Publish-AzureRmVMDscConfiguration -ConfigurationPath C:\Scripts\Configs\cmdpconfig.ps1 -ResourceGroupName $StorAcct.ResourceGroupName -ContainerName 'dscpushconfig' -StorageAccountName $StorAcct.StorageAccountName -Force
+
+$Archive = $DSCBlob.Split('/') | Select-Object -Last 1 
+
+
+
+
+
+$ArmVmRsg = Get-AzureRmResourceGroup -Name 'nrdtste'
+$ArmVm = Get-Azurermvm -ResourceGroupName $ArmVmRsg.ResourceGroupName -Name 'ctrxeusdbnp01'
+Set-AzureRmVMDscExtension -ArchiveResourceGroupName $StorAcct.ResourceGroupName -ArchiveBlobName $Archive -ResourceGroupName $ArmVm.ResourceGroupName -ArchiveStorageAccountName $StorAcct.StorageAccountName -ArchiveContainerName 'dscpushconfig' -Version '2.26' -VMName $ArmVm.Name -ConfigurationName 'CMDPConfig' -Verbose
+
+
+
+
+Get-AzureRmVMDscExtensionStatus -ResourceGroupName $ArmVm.ResourceGroupName -VMName $ArmVm.Name
+
+
+
+(Get-AzureRmVMDscExtensionStatus -ResourceGroupName $ArmVm.ResourceGroupName -VMName $Armvm.Name).DscConfigurationLog 
+
+
