d4dbba8439147e362c8db44a6660c607c84daf46
dkawula <dkawula@triconelite.com>
2017-11-27 14:23:03 -0700

    Using PowerShell to Add a Direct Member to an SCCM Collection
    I was working at a client site and was going through their server
    rollout procedure. I was quite shocked as to how many manual tasks they
    still had. One of these tasks was to add a computer directly to a SCCM
    collection. According to their requirements, they had to use direct
    membership and could not do a WMI call. So, I created the following
    script and added it to their task sequence.

---
+++ MasterPSTricksV3-Chapter5.ps1
@@ -0,0 +1,19 @@
+﻿$SccmServer = "SCCM01" 
+$PathToSCCMModule = "D:\Program Files\Microsoft Configuration Manager\AdminConsole\bin\ConfigurationManager.psd1" 
+  
+$MemberName = $env:COMPUTERNAME 
+$SCCMSession = New-PSSession -ComputerName $SccmServer 
+Invoke-Command -Session $SccmSession -ArgumentList @($PathToSCCMModule, $MemberName) -ScriptBlock { 
+    Param ( 
+        [string]$PathToSCCMModule, 
+        [string]$MemberName 
+    ) 
+    Import-Module $PathToSCCMModule -ErrorAction SilentlyContinue 
+    $SccmSite = (Get-PSDrive -PSProvider CMSite | Sort-Object -Property Name | Select-Object -First 1).Name 
+    Set-Location "$($SccmSite):" 
+  
+    $ResourceID = (Get-CMDevice -Name $MemberName).ResourceID 
+    If ($ResourceID) { 
+        Add-CMDeviceCollectionDirectMembershipRule -CollectionName "SCEP - Servers" -ResourceId $ResourceID 
+    } 
+}  
