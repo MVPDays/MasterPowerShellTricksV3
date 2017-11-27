c9d3d519b386ec779f42d1ccfb2e1b04f1325694
dkawula <dkawula@triconelite.com>
2017-11-27 15:01:09 -0700

    Use PowerShell to see how many items are in a Directory
    As you likely know, you can use Get-ChildItem to get all the items in a
    directory. Did you know, however, that you can have PowerShell quickly
    count how many files and folders there are?

---
+++ MasterPSTricksV3-Chapter18.ps1
@@ -0,0 +1,7 @@
+﻿#As you likely know, you can use Get-ChildItem to get all the items in a directory. Did you know, however, that you can have PowerShell quickly count how many files and folders there are? 
+
+(Get-ChildItem -Path c:\temp\).count  
+
+
+(Get-AdUser -filter "Name -like 'Cristal *'").count  
+
