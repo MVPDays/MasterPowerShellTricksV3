911041d1e961fb107f244132e394edfdf0d6b460
dkawula <dkawula@triconelite.com>
2017-11-27 15:06:22 -0700

    Use Test-NetConnection in PowerShell to see if a Port is Open
    The days of using ping.exe to see if a host is up or down are over. Your
    network probably shouldn’t allow ICMP to just fly around unaddressed,
    and your hosts probably shouldn’t return ICMP echo request (ping)
    messages either. So how do I know if a host is up or not?

---
+++ MasterPSTricksV3-Chapter21.ps1
@@ -0,0 +1,5 @@
+﻿#Well, it involves knowing about what your host actually does. What ports are supposed to be open? Once you know that, you can use Test-NetConnection in PowerShell to check if the port is open and responding on the host you’re interested in. 
+
+$Nodes = 'tccalst01','tccaldc04'
+$nodes
+$Nodes | % {Test-NetConnection -Computername $_.ToString() -Port 3389} 
