4b33fc38ab8c9adc905d9b2b0be75c56a7f65c2d
dkawula <dkawula@triconelite.com>
2017-11-27 14:52:55 -0700

    Dynamically Create Preset Tests for PowerShell
    The Pester people don’t really recommend this, but, I find it can be
    really helpful sometimes. What I’m talking about is dynamically creating
    assertions inside of a Pester test using PowerShell. While I think you
    should strive to follow best practices, sometimes what’s best for you
    isn’t always a best practice, and as long as you know what you’re doing,
    I think you can get away with bending the rules sometimes. Don’t tell
    anyone I said that.

---
+++ MasterPSTricksV3-Chapter13.ps1
@@ -0,0 +1,39 @@
+﻿function Get-Square { 
+    param ( 
+        [int]$Number 
+    ) 
+    $result = $Number * $Number 
+    $result 
+}  
+
+
+#This will just get the square of the number we pass it. Your test might look like this. 
+
+describe 'Get-Square' { 
+    it 'squares 1' { 
+        Get-Square 1 | Should Be 1 
+    } 
+  
+    it 'squares 2' { 
+        Get-Square 2 | Should Be 4 
+    } 
+  
+    it 'squares 3' { 
+        Get-Square 3 | Should Be 9 
+    } 
+}  
+
+#This would work. It would test your function correctly, and give you all the feedback you expect. There’s another way to do this, though. Check out this next example. 
+
+describe 'Get-Square' { 
+    $tests = @( 
+        @(1,1), 
+        @(2,4), 
+        @(3,9) 
+    ) 
+    foreach ($test in $tests) { 
+        it "squares #($test[0])" { 
+            Get-Square $test[0] | Should Be $test[1] 
+        } 
+    } 
+}  
