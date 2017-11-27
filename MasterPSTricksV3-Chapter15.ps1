7f8019ff76506f39bc1b3efa7088d921240db268
dkawula <dkawula@triconelite.com>
2017-11-27 14:55:06 -0700

    How to List All the Shares on a Server using PowerShell 
    There’s a few ways to get all of the shared folders on a server, but not
    all of them work for all versions of Windows Server. You can use the
    Get-SmbShare cmdlet, or you can make CIM/WMI do the work for you. I’ll
    show you what I prefer, though.

---
+++ MasterPSTricksV3-Chapter15.ps1
@@ -0,0 +1,13 @@
+﻿#To use Get-SmbShare on a remote computer, you’ll create a new CIM session. 
+
+$ComputerName = 'tccalst01'
+New-CimSession -ComputerName $computername -Credential $creds  
+
+#Then you can pass that CIM session to Get-SmbShare
+
+Get-SmbShare -CimSession $(get-cimsession -id 1)  
+
+#Well, luckily for those older servers, you can use Get-WmiObject to retrieve this information. 
+
+$oldcomp = 'tccalst01'
+Get-WmiObject -Class win32_share -ComputerName $oldComp -Credential $creds 
